2900d2ab25ee7798301705331123e9d7464760af
Aynacom <aynacom@hotmail.com>
2017-08-13 04:14:07 +0800

    install Clearance gem for user management

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,5 @@
+Clearance.configure do |config|
+  config.routes = true
+  config.mailer_sender = "aynacom@hotmail.com"
+  config.rotate_csrf_on_sign_in = true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'main/index'
+  root 'main#index'
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+   # get "/sign_in" => "sessions#new", as: "sign_in"
+  # delete "/sign_out" => "sessions#destroy", as: "sign_out"
+  #  get "/sign_up" => "users#new", as: "sign_up"
+  #  # post "/users" => "users#create", as: "users2"
+  # post "/users" => "users#create", as: "create"
+
+
+
+
 end
